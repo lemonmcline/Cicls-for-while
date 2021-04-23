7d5e40ff666c6bfafe072f4afe188e3adf02163e
Artyom Pishalnikov <82143842+lemonmcline@users.noreply.github.com>
2021-04-23 12:27:19 +0600

    Cicls for,while
    Cicls for,while

---
+++ Cicls for,while.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int z = 0; z <= 5;z++) {
+        cout << z << " ";
+    }
+
+    cout << endl;
+
+    for (int z = 1; z <= 5;z++) {
+        cout << z << endl;
+    }
+    cout << endl;
+
+    int x = 10;
+
+    while (x < 50)
+    {
+        cout << x << " ";
+        x = x + 100 - 90;
+    }
+    cout << endl;
+
+    int c = 0;
+
+    do {
+        cout << c << " ";
+        c = c + 100;
+    } while (c <= 1000);
+
+    cout << endl;
+
+    int c1 = 4321;
+
+    do {
+        cout << c1 << " ";
+        c1 = c1 + 50;
+    } while (c1 <= 1000);
+}
